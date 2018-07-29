8e7323ff988f9a751ae4ed46210cdf913fce5583
Austin Heller <austin.heller@hotmail.ca>
2018-07-28 20:23:54 -0400

    the first half of the framework, unfinished

---
+++ MaluigiWare/Assets/Scripts/GameManager.cs
@@ -0,0 +1,152 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    //number of lives the players have
+    [SerializeField] private int lives;
+
+    //arrays of the easy, med and hard levels, set in the editor for the game start scene
+    [SerializeField] private string[] easyLevels, medLevels, hardLevels;
+
+    //1 = easy, 2 = med, 3 = hard
+    private int difficulty;
+
+    //how many times the players have won on the current difficulty
+    private int wins;
+
+    //how many wins have to happen on the current difficulty level to pass to the next
+    //this should be atleast 3 less then the number of levels for each difficulty so that if all 3 losses happen on one difficulty, no level has to be replayed
+    [SerializeField] private int winsToProceed = 1;
+
+    //which levels have been played on the current difficulty setting, true = has been played
+    private bool[] levelsPlayed;
+
+
+    public void LevelEnded(bool didWin)
+    {
+        if(didWin)
+        {
+            wins++;
+        }
+        else
+        {
+            //if the level was lost, lose a life
+            lives -= 1;
+        }
+
+        if(lives <= 0)
+        {
+            // if no more lives, go to lose screen
+            Lose();
+        }
+
+        //do we proceed to the next difficulty option
+        if (wins >= winsToProceed)
+        {
+            //if the current difficulty level is beaten
+            wins = 0;
+            //advance difficulty forward
+            difficulty++;
+
+            if (difficulty == 2)
+            {
+                //reset the levels played for med
+                levelsPlayed = new bool[medLevels.Length - 1];
+                //if you beat the difficulty but didnt win, go to next level
+                NextLevel();
+            }
+            else if (difficulty == 3)
+            {
+                //reset the levels played for hard
+                levelsPlayed = new bool[hardLevels.Length - 1];
+                //if you beat the difficulty but didnt win, go to next level
+                NextLevel();
+            }
+            else if(difficulty > 3)
+            {
+                //you win
+                Win();
+            }
+        }
+        else
+        {
+            //if you didnt beat the difficulty yet, go to next level
+            NextLevel();
+        }
+
+
+    }
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        //players start with 3 lives and at easy difficulty
+        lives = 3;
+        difficulty = 1;
+
+        //create the levelsPlayed array with the same length as the current difficulty (easy)
+        levelsPlayed = new bool[easyLevels.Length - 1];
+	}
+
+    void NextLevel()
+    {
+        bool levelsRemain = false;
+        //first make sure that we havent played every level
+        foreach (bool level in levelsPlayed)
+        {
+            if (!level)
+                levelsRemain = true;
+        }
+
+        //if no levels remain
+        if(!levelsRemain)
+        {
+            //wipe the levels played
+            levelsPlayed = new bool[levelsPlayed.Length - 1];
+
+        }
+
+        //otherwise if the are levels left
+
+        //randomly select one
+        int levelChosen;
+        do
+        {
+            levelChosen = Random.Range(0, levelsPlayed.Length - 1);
+        } while (!levelsPlayed[levelChosen]);
+
+        //and load it
+        if (difficulty == 1)
+            SceneManager.LoadScene(easyLevels[levelChosen]);
+        else if (difficulty == 2)
+            SceneManager.LoadScene(medLevels[levelChosen]);
+        else if (difficulty == 3)
+            SceneManager.LoadScene(hardLevels[levelChosen]);
+        else
+        {
+            //getting here is a mistake, but it means you probably won
+            Win();
+        }
+
+
+
+    }
+
+    void Win()
+    {
+        print("you win :)");
+    }
+
+    void Lose()
+    {
+        print("you lose :(");
+    }
+}
---
+++ MaluigiWare/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// this class is in every scene to communicate with the gamemanager
+public class LevelManager : MonoBehaviour {
+
+    // holds a reference in the event that there isnt a game object (if someone is just running the scene)
+    [SerializeField] private GameObject gameManagerPrefab;
+
+    private GameObject gameManager;
+
+    // the length of the level, default 15, set in editor for each level
+    [SerializeField] private float LEVEL_LENGTH = 15.0f;
+
+    // the time at which the level is over
+    private float timer;
+
+
+    // called if the game is won
+    public void GameWon()
+    {
+
+    }
+
+    // called if the game is lost
+    public void GameLost()
+    {
+
+    }
+
+    void Awaken()
+    {
+        gameManager = GameObject.FindGameObjectWithTag("GameManager");
+        // check if there is a gamemanager from the last scene
+        if (gameManager == null)
+        {
+            //if there isn't, make one
+            gameManager = Instantiate(gameManagerPrefab);
+        }
+        // otherwise do nothing as we have a gamemanager
+
+    }
+
+	// use this for initialization
+	void Start ()
+    {
+
+        //start the timer
+        timer = Time.time + LEVEL_LENGTH;		
+	}
+
+    // update is called once per frame
+    void Update()
+    {
+
+        // check if the timer has run out
+        if (Time.time > timer)
+        {
+            // if it has run out, the player loses the game
+            GameLost();
+        }
+    }
+}
